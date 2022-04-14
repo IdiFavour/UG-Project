19fe192d687788f922aabbef4fe986c81f40bb34
Apple <apple@Apples-MacBook-Pro-2284.local>
2022-04-14 23:45:14 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showHide() {
+    var x = document.getElementById("showDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}

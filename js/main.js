6602b50502e3a7639344b50a5774d6332f5d5622
Idi Favour <72939160+IdiFavour@users.noreply.github.com>
2022-05-25 12:37:05 +0400

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function showHide() {
-    var x = document.getElementById("showDiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
